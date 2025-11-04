cb0dd4db98a01b960a1d6600ae38f73366b40077
cdamelincourt-resonance <c.damelincourt@resonance-firalp.fr>
2025-11-04 18:23:13 +0100

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import io
 # Télécharger le fichier CSV
 url = "https://www.data.gouv.fr/api/1/datasets/r/eb76d20a-8501-400e-b336-d85724de5435"
 response = requests.get(url)
-df = pd.read_csv(io.StringIO(response.text), sep=';', low_memory=False)
+df = pd.read_csv(io.StringIO(response.text), sep=',', low_memory=False)
 
 # Filtrer les lignes
 filtered = df[df['nom_enseigne'] == 'MobiSDEC']
