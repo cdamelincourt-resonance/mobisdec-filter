fd069b0fe044d22406439a70dd28f41c9eb0f400
cdamelincourt-resonance <c.damelincourt@resonance-firalp.fr>
2025-11-04 17:14:31 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,11 @@
 import pandas as pd
 
-# URL du fichier CSV
+# Télécharger le fichier CSV
 url = "https://www.data.gouv.fr/api/1/datasets/r/eb76d20a-8501-400e-b336-d85724de5435"
-
-# Lire le CSV directement depuis l'URL
 df = pd.read_csv(url, sep=';', low_memory=False)
 
-# Filtrer les lignes où nom_enseigne == "MobiSDEC"
+# Filtrer les lignes
 filtered_df = df[df['nom_enseigne'] == 'MobiSDEC']
 
-# Sauvegarder le résultat dans un nouveau fichier CSV
+# Enregistrer le fichier filtré
 filtered_df.to_csv("mobisdec_filtré.csv", index=False)
